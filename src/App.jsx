650f813ddca4e6ee16457aae8b8a16c0acf8c382
Jason <54602521+Jason-Wang1245@users.noreply.github.com>
2024-05-08 14:57:13 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import {useState} from 'react'
+import { useState } from "react";
 
-import ProjectsList from './components/ProjectsList'
-import AddProject from './components/AddProject'
-import NoProject from './components/NoProject'
+import ProjectsList from "./components/ProjectsList";
+import AddProject from "./components/AddProject";
+import NoProject from "./components/NoProject";
 
 function App() {
-  const [projects, setProjects] = useState([{ 
-    title: 'Test Project',
-    Description: 'A test project',
-    tasks: ['task 1', 'task 2', 'task 3']
-  }]);
+  const [projects, setProjects] = useState([
+    {
+      title: "Test Project",
+      Description: "A test project",
+      tasks: ["task 1", "task 2", "task 3"],
+    },
+  ]);
   const [noProject, setNoProject] = useState(true);
   const [curProject, setCurProject] = useState(null);
 
   return (
-    <div className='flex'>
-      <ProjectsList projects={projects}/>
+    <div className="flex">
+      <ProjectsList projects={projects} />
       {noProject && <NoProject />}
       {!noProject && <AddProject />}
     </div>
--- src/components/NoProject.jsx
+++ src/components/NoProject.jsx
@@ -1,7 +1,17 @@
-export default function NoProject(){
-    return (
-        <section className='flex-initial w-full m-4 items-center'>
-            
-        </section>
-    )
+import icon from "../assets/no-projects.png";
+
+import Button from "./Button";
+
+export default function NoProject() {
+  return (
+    <section className="flex-initial w-full m-4 items-center">
+      <img src={icon} className="w-16 h-16 mx-auto" />
+      <h2 className="font-bold mb-1 text-center text-gray-600">
+        No Project Selected
+      </h2>
+      <p className="font-light text-center text-gray-500">
+        Select a project or get started with a new one
+      </p>
+    </section>
+  );
 }
